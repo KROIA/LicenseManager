26453a08083768be0d1d447a0f685f339373f226
Alex <alexkrieg@gmx.ch>
2024-09-23 23:20:21 +0200

    -

--- examples/LicenseGenerator/src/main.cpp
+++ examples/LicenseGenerator/src/main.cpp
@@ -4,10 +4,13 @@
 
 int main(int argc, char* argv[])
 {
+	::ShowWindow(::GetConsoleWindow(), SW_HIDE);
     QApplication app(argc, argv);
 	
 #ifndef NDEBUG
 	Log::UI::createConsoleView(Log::UI::ConsoleViewType::nativeConsoleView);
+#else
+	
 #endif
 	MainWindow mainWindow;
 	mainWindow.show();
