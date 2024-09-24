cf9b267ff7bdd2cc5968bcbd278a00c1697851f5
Alex Krieg <alexkrieg@gmx.ch>
2024-09-24 10:12:27 +0200

    + High DPI scaling

--- examples/LicenseGenerator/src/main.cpp
+++ examples/LicenseGenerator/src/main.cpp
@@ -5,7 +5,14 @@
 int main(int argc, char* argv[])
 {
 	::ShowWindow(::GetConsoleWindow(), SW_HIDE);
+#if QT_VERSION >= QT_VERSION_CHECK(5, 14, 0)
+	// Enable scaling for high resolution displays
+	QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+	QGuiApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+	QGuiApplication::setHighDpiScaleFactorRoundingPolicy(Qt::HighDpiScaleFactorRoundingPolicy::PassThrough);
+#endif
     QApplication app(argc, argv);
+
 	
 #ifndef NDEBUG
 	Log::UI::createConsoleView(Log::UI::ConsoleViewType::nativeConsoleView);
